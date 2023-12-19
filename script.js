c38f6e541120eaba8b86902994432b8bb2dd383d
Agustin <ozzon42@gmail.com>
2023-12-19 19:47:52 -0300

    comments

--- script.js
+++ script.js
@@ -10,9 +10,13 @@ const main = async (values) => {
     let obj = {}
     let game, balls;
     let colors = {} 
+
+    // Itera sobre cada linea del input
     for (let i = 0; i < content.length; i++) {
         [game, balls] = content[i].split(':');
         balls = balls.split(';');
+
+        // Itera sobre cada resultado de sacar pelotas del saco en un mismo game 
         for (let j = 0; j < balls.length; j++) {
             balls[j] = balls[j].split(',');
             colors[j] = {
@@ -20,9 +24,10 @@ const main = async (values) => {
                 green: 0,
                 red: 0
             }
+            
+            // Itera sobre cada color de las pelotas que se sacaron a la vez
             for (let k = 0; k < balls[j].length; k++) {
                 balls[j][k] = balls[j][k].trim();
-                console.log(balls[j][k])
                 if(balls[j][k].indexOf('blue') != -1){
                     colors[j].blue = parseInt(balls[j][k].split(' ')[0])
                 }
@@ -33,12 +38,13 @@ const main = async (values) => {
                     colors[j].red = parseInt(balls[j][k].split(' ')[0])
                 }   
             }
+            // Se marca el game si tiene mas pelotas de algun color como imposible (null)
             if(values.blue < colors[j].blue || values.green < colors[j].green || values.red < colors[j].red){
                 obj[game.split(' ')[1]] = null;
             }
-            console.log(colors[j])
-            console.log(values)
         }
+
+        // Si esta marcado => se elimina del resultado
         if(obj[game.split(' ')[1]] !== null){
             obj[game.split(' ')[1]] = colors;
         }
@@ -47,6 +53,10 @@ const main = async (values) => {
         }
         
     }
-    console.log(Object.keys(obj).reduce((p,c)=> parseInt(p) + parseInt(c) ));
+    // Get each key of the object (games' number) and get their sum
+    return Object.keys(obj).reduce((p,c)=> parseInt(p) + parseInt(c));
 }
-main({blue: 14, green: 13, red: 12})
+let result = main({blue: 14, green: 13, red: 12})
+console.log(result)
+
+
