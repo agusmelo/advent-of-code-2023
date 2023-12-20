b5603a7fc419b00766ae98948ab2c8bc8cfb4997
Agustin <ozzon42@gmail.com>
2023-12-19 23:02:19 -0300

    inprogress

--- script.js
+++ script.js
@@ -9,13 +9,17 @@ const main = async (values = {}) => {
     content = content.split('\r\n')
     let obj = {}
     let game, balls;
-    let colors = {} 
+    let colors, minColors = []
 
     // Itera sobre cada linea del input
     for (let i = 0; i < content.length; i++) {
         [game, balls] = content[i].split(':');
         balls = balls.split(';');
-
+        minColors[i] = {
+            blue: 0,
+            green: 0,
+            red: 0
+        }
         // Itera sobre cada resultado de sacar pelotas del saco en un mismo game 
         for (let j = 0; j < balls.length; j++) {
             balls[j] = balls[j].split(',');
@@ -24,7 +28,6 @@ const main = async (values = {}) => {
                 green: 0,
                 red: 0
             }
-            
             // Itera sobre cada color de las pelotas que se sacaron a la vez
             for (let k = 0; k < balls[j].length; k++) {
                 balls[j][k] = balls[j][k].trim();
@@ -38,8 +41,13 @@ const main = async (values = {}) => {
                     colors[j].red = parseInt(balls[j][k].split(' ')[0])
                 }   
             }
+
+            // minColors[i].blue = Math.max(minColors.blue | 0, colors.blue);
+            // minColors[i].green = Math.max(minColors.green | 0, colors.green);
+            // minColors[i].red = Math.max(minColors.red | 0, colors.red);
+
             // Se marca el game si tiene mas pelotas de algun color como imposible (null)
-            if(values != {} && (values.blue < colors[j].blue || values.green < colors[j].green || values.red < colors[j].red)){
+            if(Object.keys(values).length !== 0 && (values.blue < colors[j].blue || values.green < colors[j].green || values.red < colors[j].red)){
                 obj[game.split(' ')[1]] = null;
             }
         }
@@ -54,6 +62,8 @@ const main = async (values = {}) => {
         
     }
     // Get each key of the object (games' number) and get their sum
+    console.log(minColors.reduce( (p,c)=> p + (c.blue*c.green*c.red)))
+
     return Object.keys(obj).reduce((p,c)=> parseInt(p) + parseInt(c));
 }
 let result = main({blue: 14, green: 13, red: 12})
