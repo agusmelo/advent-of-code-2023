c0e4b39239f7a8913f9574e6ec92add9d8469f0b
Agustin <amagustinmelo8@gmail.com>
2024-01-07 16:52:59 -0300

    day 9 part 2 complete ✅

--- day9.js
+++ day9.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises');
 
+const part = 2;
 
 const main = async () =>{
     let rawData = await fs.readFile('./input.txt',{encoding: 'utf-8'});
@@ -18,12 +19,28 @@ const main = async () =>{
             }
             j++
         }
-        subRows[subRows.length-1].push(0)
-        // Extrapolacion
-        for (let j = subRows.length -2 ; j >= 0 ; j--) {
-            subRows[j].push(subRows[j][subRows[j].length-1] + subRows[j+1][subRows[j+1].length-1])
+
+        if(part === 2 ){
+            console.log('PART 2')
+            subRows[subRows.length-1].unshift(0)
+            // Extrapolacion
+            for (let j = subRows.length -2 ; j >= 0 ; j--) {
+                subRows[j].unshift(subRows[j][0] - subRows[j+1][0])
+            }
+            sum += subRows[0][0] 
+
         }
-        sum += subRows[0][subRows[0].length-1] 
+        else{
+            subRows[subRows.length-1].push(0)
+            // Extrapolacion
+            for (let j = subRows.length -2 ; j >= 0 ; j--) {
+                subRows[j].push(subRows[j][subRows[j].length-1] + subRows[j+1][subRows[j+1].length-1])
+            }
+            sum += subRows[0][subRows[0].length-1] 
+        }
+        
+        
+        console.log(subRows[0])
         subRows = [];
     }
     console.log('SUMA: ',sum)
