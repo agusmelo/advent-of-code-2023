4bc382e22d62edb543a4724c03e9ec94389e92a2
Agustin <amagustinmelo8@gmail.com>
2024-01-14 21:34:50 -0300

    day 10 part 2 (WIP) �

--- day10.js
+++ day10.js
@@ -1,4 +1,4 @@
-const {readFileSync} = require('fs')
+const {readFileSync, writeFileSync} = require('fs')
 
 const getEdges = (rows, explored, nodePosX, nodePosY) => {
     let validEdges = [];
@@ -22,13 +22,13 @@ const bfs = (rows, start) => {
     let layers = [[]]; // {x,y}
     let explored = [[]];
     let adyNodes;
-    // let parent = {};
+    let parent = {};
     for (let i = 0; i < rows.length; i++) { 
         explored[i] = new Array(rows[i].length).fill(false);
     }
     explored[start.x][start.y] = true;
     layers[0] = [start]
-    // parent[`${start.x}${start.y}`] = null;
+    parent[`${start.x}${start.y}`] = null;
     
     let i = 0
     while(layers[i].length > 0){
@@ -38,14 +38,16 @@ const bfs = (rows, start) => {
             for ( let key in  adyNodes ) {
                 if(!explored[adyNodes[key].x][adyNodes[key].y]){
                     explored[adyNodes[key].x][adyNodes[key].y] = true;
-                    // parent[`${adyNodes[key].x}${adyNodes[key].y}`] = v;
-                    layers[i+1].push(adyNodes[key]);
+                    parent[`${adyNodes[key].x}${adyNodes[key].y}`] = v;
+                    console.log(rows[v.x][v.y], `(${v.x},${v.y})`);
+                    rows[v.x] = rows[v.x].substring(0,v.y) + i + rows[v.x].substring(v.y+1);
+                    layers[i+1].push(adyNodes[key]);    
                 }
             }
         }
         i++; 
     }
-    return layers;
+    return [layers,parent];
 }
 
 
@@ -66,8 +68,48 @@ const main = () => {
         i++;
     }
     
-    //BFS   
-    console.log(bfs(rows, start).length-2); // Substract 2 to get the last index
+    //BFS
+    let layers, parent;
+    [layers, parent] = bfs(rows, start)
+    console.log(layers.length-2); // Substract 2 to get the last index
+    let polarity = false;
+    let count = 0;
+    let subcount = 0;
+    let test = ''
+    let startPointer = -1;
+    let finishPointer = -1;
+    for (let i = 0; i < rows.length; i++) {
+        subcount = 0;
+        polarity = false;
+        startPointer = -1;
+        finishPointer = -1;
+        for (let j = 0; j < rows[i].length; j++){
+            if(parent.hasOwnProperty(`${i}${j}`) && rows[i][j] === '-'){
+                polarity = !polarity;
+            }
+            else if(parent.hasOwnProperty(`${i}${j}`) && ['|','J','7','F','L'].includes(rows[i][j])){
+                polarity = !polarity;
+                count += subcount;
+                if(startPointer != -1 && subcount != 0){
+                    rows[i] = rows[i].substring(0,startPointer+1) + 'I'.repeat(j -startPointer-1) + rows[i].substring(j);
+                }
+                subcount = 0;
+                startPointer = j;
+            }
+            else if(polarity && !parent.hasOwnProperty(`${i}${j}`)){
+                subcount++;
+            }
+            test += polarity ? '1':'0';
+        }
+        test +='\n'
+        // count += subcount;
+    }
+    console.log(test)
+    console.log(count)
+    console.log(rows.join('\n'));
+    
+
+    
 }
 
 main()
